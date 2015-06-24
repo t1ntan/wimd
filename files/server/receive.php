aac5b99ba93e35920dcee08f28857b64174589a9
Stephan Renner <stephan_renner@mailbox.org>
2015-06-24 19:46:25 +0200

    unterstuetzung fuer wlan0 hinzugefuegt

--- files/server/receive.php
+++ files/server/receive.php
@@ -3,13 +3,15 @@
  * in diesem Script verwendete Variablen
  */
 $clean_hostname = "";
-$clean_ip = "";
-$clean_mac = "";
+$clean_eth0_mac = "";
+$clean_eth0_ip4 = "";
+// $clean_eth0_ip6 = "";
+$clean_wlan0_mac = "";
+$clean_wlan0_ip4 = "";
+// $clean_wlan0_ip6 = "";
+
 
 $datei = 'devices.csv';
-
-$f_exist = 0;
-$f_writable = 0;
 
 /*
  * Prüfen der Daten die per GET übergebenen werden
@@ -39,46 +41,90 @@ if (isset($_GET['hostname'])) {
         $clean_hostname = "hostname-Variable fehlt!";
 } // ende vorhanden-prüfung
 
-// prüfen ob ip in $_GET vorhanden ist.
-if (isset($_GET['ip'])) {
 
-    // prüfen ob ip nicht leer ist.
-    if (!empty($_GET['ip'])) {
+// prüfen ob eth0_mac in $_GET vorhanden ist.
+if (isset($_GET['eth0_mac'])) {
 
-        // prüfen ob ip eine gültige IPv4-Nummer ist.
-        if (preg_match('/^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})){3})$/', $_GET['ip'])) {
-            $clean_ip = $_GET['ip'];
+    // prüfen ob eth0_mac nicht leer ist.
+    if (!empty($_GET['eth0_mac'])) {
+
+        // prüfen ob eth0_mac eine gültige MAC-Adresse enthält
+          if (preg_match('/^(([0-9a-fA-F][0-9a-fA-F])([:\-][0-9a-fA-F][0-9a-fA-F]){5})$/', $_GET['eth0_mac'])) {
+            $clean_eth0_mac = $_GET['eth0_mac'];
         } else {
-            $clean_ip = "ip-Formatfehler";
-        } // ende der regex-prüfung
-
-    } else {
-        $clean_ip = "ip-Variable ist leer!";
-    } // ende leer-prüfung
-
-} else {
-        $clean_ip = "ip-Variable fehlt!";
-} // ende vorhanden-prüfung
-
-// prüfen ob mac in $_GET vorhanden ist.
-if (isset($_GET['mac'])) {
-
-    // prüfen ob mac nicht leer ist.
-    if (!empty($_GET['mac'])) {
-
-        // prüfen ob mac eine gültige MAC-Adresse enthält
-          if (preg_match('/^(([0-9a-fA-F][0-9a-fA-F])([:\-][0-9a-fA-F][0-9a-fA-F]){5})$/', $_GET['mac'])) {
-            $clean_mac = $_GET['mac'];
-        } else {
-            $clean_mac = "mac-Formatfehler";
+            $clean_eth0_mac = "eth0_mac-Formatfehler";
         } // ende regex-prüfung
 
     } else {
-        $clean_mac = "mac-Variable ist leer!";
+        $clean_eth0_mac = "eth0_mac-Variable ist leer!";
+    } // ende leer-prüfung
+} else {
+        $clean_eth0_mac = "eth0_mac-Variable fehlt!";
+} // ende vorhanden-prüfung
+
+
+// prüfen ob eth0_ipv4 in $_GET vorhanden ist.
+if (isset($_GET['eth0_ipv4'])) {
+
+    // prüfen ob eth0_ipv4 nicht leer ist.
+    if (!empty($_GET['eth0_ipv4'])) {
+
+        // prüfen ob eth0_ipv4 eine gültige IPv4-Nummer ist.
+        if (preg_match('/^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})){3})$/', $_GET['eth0_ipv4'])) {
+            $clean_eth0_ipv4 = $_GET['eth0_ipv4'];
+        } else {
+            $clean_eth0_ipv4 = "eth0_ipv4-Formatfehler";
+        } // ende der regex-prüfung
+
+    } else {
+        $clean_eth0_ipv4 = "eth0_ipv4-Variable ist leer!";
     } // ende leer-prüfung
 
 } else {
-        $clean_mac = "mac-Variable fehlt!";
+        $clean_eth0_ipv4 = "eth0_ipv4-Variable fehlt!";
+} // ende vorhanden-prüfung
+
+
+// prüfen ob wlan0_mac in $_GET vorhanden ist.
+if (isset($_GET['wlan0_mac'])) {
+
+    // prüfen ob wlan0_mac nicht leer ist.
+    if (!empty($_GET['wlan0_mac'])) {
+
+        // prüfen ob wlan0_mac eine gültige MAC-Adresse enthält
+          if (preg_match('/^(([0-9a-fA-F][0-9a-fA-F])([:\-][0-9a-fA-F][0-9a-fA-F]){5})$/', $_GET['wlan0_mac'])) {
+            $clean_wlan0_mac = $_GET['wlan0_mac'];
+        } else {
+            $clean_wlan0_mac = "wlan0_mac-Formatfehler";
+        } // ende regex-prüfung
+
+    } else {
+        $clean_wlan0_mac = "wlan0_mac-Variable ist leer!";
+    } // ende leer-prüfung
+} else {
+        $clean_wlan0_mac = "wlan0_mac-Variable fehlt!";
+} // ende vorhanden-prüfung
+
+
+// prüfen ob wlan0_ipv4 in $_GET vorhanden ist.
+if (isset($_GET['wlan0_ipv4'])) {
+
+    // prüfen ob wlan0_ipv4 nicht leer ist.
+    if (!empty($_GET['wlan0_ipv4'])) {
+
+        // prüfen ob wlan0_ipv4 eine gültige IPv4-Nummer ist.
+        if (preg_match('/^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})){3})$/', $_GET['wlan0_ipv4'])) {
+            $clean_wlan0_ipv4 = $_GET['wlan0_ipv4'];
+        } else {
+            $clean_wlan0_ipv4 = "wlan0_ipv4-Formatfehler";
+        } // ende der regex-prüfung
+
+    } else {
+        $clean_wlan0_ipv4 = "wlan0_ipv4-Variable ist leer!";
+    } // ende leer-prüfung
+
+} else {
+        $clean_wlan0_ipv4 = "wlan0_ipv4-Variable fehlt!";
 } // ende vorhanden-prüfung
 
 
@@ -101,7 +147,7 @@ $zeit = date("H:i:s", $timestamp);
  */
 
 // zu speichernden Datensatz erstellen
-$datensatz = "'".$timestamp."';'".$datum."';'".$zeit."';'".$clean_mac."';'".$clean_hostname."';'".$clean_ip."'"."<br />\n";
+$datensatz = "'".$datum."';'".$zeit."';'".$clean_hostname."';'".$clean_eth0_mac."';'".$clean_eth0_ipv4."';'".$clean_wlan0_mac."';'".$clean_wlan0_ipv4."'"."<br />\n";
 
 // prüfen ob die Datei devices.csv existiert
 $handle = fopen($datei, a);
